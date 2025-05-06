012facefa25202a974c65c19e3e3f18bd28a1787
ebuo <57754835+ebuojr@users.noreply.github.com>
2025-05-06 15:07:39 +0200

    Refactor ReorderServiceTest to remove unused service dependencies and simplify constructor

--- ReStockApiTest/ReorderServiceTest.cs
+++ ReStockApiTest/ReorderServiceTest.cs
@@ -1,32 +1,9 @@
-﻿using ReStockApi.Services.Inventory;
-using ReStockApi.Services.Product;
-using ReStockApi.Services.Reorder;
-using ReStockApi.Services.ReorderLog;
-using ReStockApi.Services.Store;
-using System.Security.Cryptography;
-
-namespace ReStockApiTest
+﻿namespace ReStockApiTest
 {
     
     public class ReorderServiceTest
     {
-        private readonly IReorderService _reorderService;
-        private readonly IReorderLogService _reorderLogService;
-        private readonly IProductService _productService;
-        private readonly IStoreService _storeService;
-        private readonly IInventoryService _inventoryService;
-
-        public ReorderServiceTest(IReorderService reorderService, IReorderLogService reorderLogService, IProductService productService, IStoreService storeService, IInventoryService inventoryService)
-        {
-            _reorderService = reorderService;
-            _reorderLogService = reorderLogService;
-            _productService = productService;
-            _storeService = storeService;
-            _inventoryService = inventoryService;
-        }
-
-        [Fact]
-        public async Task CreatePotentialOrdersByStoreNoAsync_StoreDoesNotExists()
+        public ReorderServiceTest()
         {
 
         }

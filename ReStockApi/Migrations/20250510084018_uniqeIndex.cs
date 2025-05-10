4faa7023bf350d49d34d4d07ae48575faa478a09
ebuo <57754835+ebuojr@users.noreply.github.com>
2025-05-10 10:56:20 +0200

    Add unique indexes to various tables for data integrity

---
+++ ReStockApi/Migrations/20250510084018_uniqeIndex.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReStockApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class uniqeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Stores_No",
+                table: "Stores",
+                column: "No",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoreInventories_StoreNo_ItemNo",
+                table: "StoreInventories",
+                columns: new[] { "StoreNo", "ItemNo" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesOrders_HeaderNo",
+                table: "SalesOrders",
+                column: "HeaderNo",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesOrderLines_HeaderNo_LineNo",
+                table: "SalesOrderLines",
+                columns: new[] { "HeaderNo", "LineNo" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reorders_StoreNo_ItemNo",
+                table: "Reorders",
+                columns: new[] { "StoreNo", "ItemNo" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ItemNo",
+                table: "Products",
+                column: "ItemNo",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryThresholds_StoreNo_ItemNo",
+                table: "InventoryThresholds",
+                columns: new[] { "StoreNo", "ItemNo" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DistributionCenterInventories_ItemNo",
+                table: "DistributionCenterInventories",
+                column: "ItemNo",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Stores_No",
+                table: "Stores");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StoreInventories_StoreNo_ItemNo",
+                table: "StoreInventories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SalesOrders_HeaderNo",
+                table: "SalesOrders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SalesOrderLines_HeaderNo_LineNo",
+                table: "SalesOrderLines");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reorders_StoreNo_ItemNo",
+                table: "Reorders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ItemNo",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InventoryThresholds_StoreNo_ItemNo",
+                table: "InventoryThresholds");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DistributionCenterInventories_ItemNo",
+                table: "DistributionCenterInventories");
+        }
+    }
+}
--- ReStockApi/Models/ReStockDbContext.cs
+++ ReStockApi/Models/ReStockDbContext.cs
@@ -14,19 +14,35 @@ namespace ReStockApi.Models
         public DbSet<SalesOrder> SalesOrders { get; set; }
         public DbSet<SalesOrderLine> SalesOrderLines { get; set; }
         public DbSet<Reorder> Reorders { get; set; }
-        public DbSet<ReOrderLog> ReOrderLogs  { get; set; }
+        public DbSet<ReOrderLog> ReOrderLogs { get; set; }
         public DbSet<JobLastRun> JobLastRuns { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Product>().HasIndex(p => p.ItemNo).IsUnique();
+
             modelBuilder.Entity<Store>().HasKey(s => s.Id);
+            modelBuilder.Entity<Store>().HasIndex(s => s.No).IsUnique();
+
             modelBuilder.Entity<StoreInventory>().HasKey(si => si.Id);
+            modelBuilder.Entity<StoreInventory>().HasIndex(si => new { si.StoreNo, si.ItemNo }).IsUnique();
+
             modelBuilder.Entity<DistributionCenterInventory>().HasKey(dci => dci.Id);
+            modelBuilder.Entity<DistributionCenterInventory>().HasIndex(dci => dci.ItemNo).IsUnique();
+
             modelBuilder.Entity<InventoryThreshold>().HasKey(it => it.Id);
+            modelBuilder.Entity<InventoryThreshold>().HasIndex(it => new { it.StoreNo, it.ItemNo }).IsUnique();
+
             modelBuilder.Entity<SalesOrder>().HasKey(so => so.Id);
+            modelBuilder.Entity<SalesOrder>().HasIndex(so => so.HeaderNo).IsUnique();
+
             modelBuilder.Entity<SalesOrderLine>().HasKey(sol => sol.Id);
+            modelBuilder.Entity<SalesOrderLine>().HasIndex(sol => new { sol.HeaderNo, sol.LineNo }).IsUnique();
+
             modelBuilder.Entity<Reorder>().HasKey(r => r.Id);
+            modelBuilder.Entity<Reorder>().HasIndex(r => new { r.StoreNo, r.ItemNo }).IsUnique();
+
             modelBuilder.Entity<ReOrderLog>().HasKey(rol => rol.Id);
             modelBuilder.Entity<JobLastRun>().HasKey(jlr => jlr.Id);
         }

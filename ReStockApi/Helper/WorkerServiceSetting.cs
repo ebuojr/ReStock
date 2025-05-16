e33006567aa67fc6528711a152ddc5947e2e9a94
ebuo <57754835+ebuojr@users.noreply.github.com>
2025-05-16 15:20:37 +0200

    Update WorkerServiceSetting readiness and db changes
    Changed the `IsReadyToRun` property in the `WorkerServiceSetting` class from `true` to `false`, indicating the service is not ready to run by default. Additionally, there are binary changes in the `restock.db` file, reflecting updates to its contents.

--- ReStockApi/Helper/WorkerServiceSetting.cs
+++ ReStockApi/Helper/WorkerServiceSetting.cs
@@ -3,7 +3,7 @@ namespace ReStockApi.Helper
 {
     public class WorkerServiceSetting
     {
-        public static bool IsReadyToRun { get; set; } = true;
+        public static bool IsReadyToRun { get; set; } = false;
         public static bool IsRunning { get; set; } = false;
         public static DateTime LastRun { get; set; } = DateTime.MinValue;
     }
